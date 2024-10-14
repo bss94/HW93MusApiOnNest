e058a4e4eac9ebd27c860efd2cf78abd00073ce8
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-10-14 15:40:09 +0600

    HW94 create login on user controller create auth service add to app module install passport passport-locale @types/passport @nestjs/passport

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -18,5 +19,11 @@ export class UsersController {
     user.generateToken();
 
     return await user.save();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('sessions')
+  async login(@Req() req: Request) {
+    return req.user;
   }
 }
